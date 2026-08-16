fe7d7c3b3ab4c735e01f39b24f90455fef47021c
Abowd123 <163924398+Abowd123@users.noreply.github.com>
2026-08-17 02:17:03 +0300

    Upload via ZIP to GitHub Uploader (Telegram bot)

---
+++ VT_Patcher/Utils/Credits.py
@@ -0,0 +1,32 @@
+from ..ANSI_COLORS import ANSI; C = ANSI()
+
+def Credits():
+    exit(f"""
+{C.R}                 \u2620  CREDITS  \u2620
+
+{C.S} Developer {C.E}{C.OG}  \U0001d75f\U0001f5b8\U0001f5f3\U0001f5f0\u0970\U0001d75c4 {C.S}{C.P} @VT_YC {C.E}
+
+{C.S} Channel {C.E}{C.OG}    @VT_YC {C.S}{C.P} https://t.me/VT_YC {C.E}
+
+{C.S} Flutter SSL & TG Patch {C.E}{C.G} AbhiTheM0dder {C.S}{C.P} @AbhiTheM0dder {C.E}
+
+{C.S} Pine Hook {C.E}{C.G} AbhiTheM0dder | canyie {C.S}{C.P} @AbhiTheM0dder | @canyie {C.E}
+
+{C.S} MITM {C.E}{C.G} Niklas Higi {C.S}{C.P} https://github.com/shroudedcode/apk-mitm {C.E}
+
+{C.S} APKTool {C.E}{C.G} Connor Tumbleson {C.S}{C.P} @iBotPeaches {C.E}
+
+{C.S} APKEditor {C.E}{C.G} REAndroid {C.S}{C.P} @kikfox {C.E}
+
+{C.S} Uber-Apk-Signer {C.E}{C.G} Patrick Favre {C.S}{C.P} @patrickfav {C.E}
+
+{C.S} Original Base {C.E}{C.G} RK Techno India {C.S}{C.P} @rktechnoindians {C.E}
+
+{C.R}  \u2620  VT_Patcher by \U0001d75f\U0001f5b8\U0001f5f3\U0001f5f0\u0970\U0001d75c4 \u2620
+{C.R}  ===============================
+{C.S}  Channel {C.E}{C.OG} @VT_YC
+{C.S}  Username {C.E}{C.OG} @VT_YC
+{C.S}  Telegram {C.E}{C.OG} https://t.me/VT_YC
+
+{C.Y}  Please Maintain Our Credits.
+    """)
